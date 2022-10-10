4d443572c17d4b7ee2f1e6de1ab3226a081c8335
GvidoNN <khodosok.2004@gmail.com>
2022-10-10 22:41:05 +0300

    done with onClickListener in "Maps"

--- app/src/main/java/com/example/theescapists2/FragmentMap.kt
+++ app/src/main/java/com/example/theescapists2/FragmentMap.kt
@@ -1,6 +1,7 @@
 package com.example.theescapists2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -106,7 +107,6 @@ class FragmentMap : Fragment(), MapAdapter.Listener{
     }
 
     override fun onClick(map: Maps) {
-        super.onClick(map)
-        Toast.makeText(this,"Нажали на ${map.mapName}", Toast.LENGTH_SHORT).show()
+        Log.d("MyLog","Нажали на ${map.mapName}")
     }
 }
